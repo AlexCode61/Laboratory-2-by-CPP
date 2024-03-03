c10edaf301783cd6ef04c98540fb380571eba999
RedFront-site <sasha200509@mail.ru>
2024-03-04 02:59:25 +0300

    Add Task

---
+++ Lab_2_task_1/Lab_2_task_1/Lab_2_task_1.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <random>
+using namespace std;
+
+int main()
+{
+	srand(time(NULL));
+	int A[10],raz;
+	int max = 15;
+	int min = -20;
+
+	for (int i = 0; i < 10; ++i) {
+		*(A + i) = rand()%(max - min+1) - abs(min);
+	}
+
+	if (*A >= 0) {
+		raz = *A;
+		cout << *A << " Polozhitelnoe\n";
+	}
+	else {
+		cout << *A << " Otricatelnoe\n";
+	}
+	for (int i = 1; i < 10; ++i) {
+		if (*(A + i) >= 0) {
+			cout << *(A + i) << " Polozhitelnoe\n";
+			raz = raz - *(A + i);
+			cout << raz << " Raznostb\n";
+		}
+		else {
+			cout << *(A + i) << " Otricatelnoe\n";
+		}
+	}
+	cout << raz << " Raznostb\n";
+	return 0;
+}
+
---
+++ Lab_2_task_2/Lab_2_task_2/Lab_2_task_2.cpp
@@ -0,0 +1,59 @@
+﻿
+#include <iostream>
+#include <random>
+#include <time.h>
+using namespace std;
+
+int main()
+{
+	srand(static_cast<unsigned int>(time(NULL)));
+	int size;
+	int index = 0;
+	double prouz = 1;
+	double sum = 0;
+	double max = -99999999;
+
+	cout << "Array length\n";
+	cin >> size;
+
+	if (cin.fail() or size <= 1) {
+		cout << "Error Array!";
+		return 0;
+	}
+
+	double* arr = new double(size);
+	double* arr_reverse = new double(size);
+
+	cout << "Original array\n";
+
+	for (int i = 0; i < size; ++i) {
+		*(arr + i) = static_cast<double>(rand() % 100) / 10.0;
+		cout << *(arr + i) << "/";
+	}
+
+	for (int i = 0; i < size; ++i) {
+		if (*(arr + i) > max) {
+			max = *(arr + i);
+			index = i;
+		}
+		if (*(arr + i) < 0) {
+			prouz = *(arr + i) * prouz;
+		}
+	}
+
+	for (int i = 0; i <= index; ++i) {
+		if (*(arr + i) > 0) {
+			sum = sum + *(arr + i);
+
+		}
+	}
+	cout << "\nReverse\n";
+
+	for (int i = size-1; i >= 0; --i) {
+		*(arr_reverse+(i-size)) = *(arr + i);
+		cout << *(arr + i) << "/";
+	}
+
+	cout << "\nSumma PolozhitelnbIX\n";
+	cout << sum;
+}
